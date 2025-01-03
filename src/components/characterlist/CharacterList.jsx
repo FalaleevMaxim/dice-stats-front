6f6b0d0430516cca9441d6b6d194445df3123bb2
FalaleevMaxim <maximfalaleev@mail.ru>
2025-01-04 01:38:26 +0400

    Исправление стилей

--- src/components/characterlist/CharacterList.jsx
+++ src/components/characterlist/CharacterList.jsx
@@ -4,6 +4,7 @@ import classes from "./CharacterList.module.css";
 function CharacterList({refreshKey, onSelectionChange}) {
     const [characters, setCharacters] = useState([])
     const [selected, setSelected] = useState(null)
+    const [search, setSearch] = useState("")
 
     async function fetchCharacters() {
         const response = await fetch("http://localhost:8080/character/all")
@@ -21,16 +22,32 @@ function CharacterList({refreshKey, onSelectionChange}) {
         onSelectionChange(newSelection)
     }
 
+    function handleSearchChange(e) {
+        setSearch(e.target.value);
+        if (selected) {
+            setSelected(null)
+            onSelectionChange(null)
+        }
+    }
+
     return (
-        <ul className={classes.characterList}>
-            {characters.map((d) => (
-                <li key={d.id}
-                    className={selected === d.id ? classes.active : ""}
-                    onClick={() => changeSelection(d.id)}>
-                    {d.name}
-                </li>
-            ))}
-        </ul>
+        <div className={classes.characterContainer}>
+            <input
+                type="text"
+                value={search}
+                onChange={handleSearchChange}
+                placeholder="Поиск персонажей..."
+            />
+            <ul className={classes.characterList}>
+                {characters.map((d) => (
+                    <li key={d.id}
+                        className={!d.name.toUpperCase().includes(search.toUpperCase()) ? classes.disable : selected === d.id ? classes.active : ""}
+                        onClick={() => changeSelection(d.id)}>
+                        {d.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
--- src/components/dicelist/DiceList.jsx
+++ src/components/dicelist/DiceList.jsx
@@ -4,6 +4,7 @@ import classes from "./DiceList.module.css";
 function DiceList({refreshKey, onSelectionChange}) {
     const [dice, setDice] = useState([])
     const [selected, setSelected] = useState(null)
+    const [search, setSearch] = useState("")
 
     async function fetchDice() {
         const response = await fetch("http://localhost:8080/dice/all")
@@ -21,17 +22,33 @@ function DiceList({refreshKey, onSelectionChange}) {
         onSelectionChange(newSelection)
     }
 
+    function handleSearchChange(e) {
+        setSearch(e.target.value);
+        if (selected) {
+            setSelected(null)
+            onSelectionChange(null)
+        }
+    }
+
     return (
-        <ul className={classes.diceList}>
-            {dice.map((d) => (
-                <li key={d.id}
-                    className={selected === d.id ? classes.active : ""}
-                    onClick={() => changeSelection(d.id)}>
-                    <img src={d.image} alt={d.name}/>
-                    <span>{d.name}</span>
-                </li>
-            ))}
-        </ul>
+        <div className={classes.diceContainer}>
+            <input
+                type="text"
+                value={search}
+                onChange={handleSearchChange}
+                placeholder="Поиск кубиков..."
+            />
+            <ul className={classes.diceList}>
+                {dice.map((d) => (
+                    <li key={d.id}
+                        className={!d.name.toUpperCase().includes(search.toUpperCase()) ? classes.disable : selected === d.id ? classes.active : ""}
+                        onClick={() => changeSelection(d.id)}>
+                        <img src={d.image} alt={d.name}/>
+                        <span>{d.name}</span>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
 
